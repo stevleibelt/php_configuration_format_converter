d556ab2e7f3a59b7b1edfe19897731554d908009
stevleibelt <stev@leibelt.de>
2013-06-07 00:16:41 +0200

    Created example directory.

---
+++ example/fromYamlToPhp.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Converted configuration from source "configtest.yaml"
+ *
+ * @author net_bazzline/php_component_converter
+ * @since 2013-06-07
+ */
+
+return array (
+  'foo' => 
+  array (
+    'bar' => 'foobar',
+    'barz' => 'foobarz',
+  ),
+  'fup' => 'fuppes',
+)
