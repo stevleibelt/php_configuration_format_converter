c844955a28c041d3225dfe105c321ab33e6e1afb
stevleibelt <stev@leibelt.de>
2013-06-07 23:04:41 +0200

    Fixed bug in json example source

--- example/json/destinationFromJsonToPhp.php
+++ example/json/destinationFromJsonToPhp.php
@@ -1,10 +1,28 @@
 <?php
 /**
- * Converted configuration from source "example/sourceFromJsonToPhp.json"
+ * Converted configuration from source "example/json/sourceFromJsonToPhp.json"
  *
  * @author net_bazzline/php_component_converter
  * @since 2013-06-07
  */
 
 return array (
+  'foo' => 
+  array (
+    'bar' => 'foobar',
+    'barz' => 'foobarz',
+    'anotherArray' => 
+    array (
+      0 => '--',
+      10 => '42',
+      99 => '13',
+    ),
+    'arrayWithKeys' => 
+    array (
+      0 => 'foo',
+      1 => 'bar',
+      2 => 'foobar',
+    ),
+  ),
+  'fup' => 'fuppes',
 );
