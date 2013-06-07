66256a01defc28b17c87ec9298f82ef72ac87c39
stevleibelt <stev@leibelt.de>
2013-06-07 13:35:31 +0200

    Prefixed examples with source or destination to clear things up.

---
+++ example/sourceFromPhpToPhp.php
@@ -0,0 +1,22 @@
+<?php
+
+return array (
+  'foo' => 
+  array (
+    'bar' => 'foobar',
+    'barz' => 'foobarz',
+    'anotherArray' => 
+    array (
+      0 => '--',
+      10 => 42,
+      99 => 13,
+    ),
+    'arrayWithKeys' => 
+    array (
+      0 => 'foo',
+      1 => 'bar',
+      2 => 'foobar',
+    ),
+  ),
+  'fup' => 'fuppes',
+)
